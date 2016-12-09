640a0458e6fe0855d93483083d8b8a557ab7c8c8
Erica  Glimsholt <ericaglimsholt@gmail.com>
2016-12-09 17:10:49 +0100

    Added action to right links and added a button called new user

--- blocks/header.php
+++ blocks/header.php
@@ -2,10 +2,22 @@
 
   <img src="img/logo-linkify.png" alt="Linkify Logotype" />
 
-    <form class="headerForm" action="index.html" method="post">
+  <div class="headerForm">
+
+
+
+    <form class="loginform" action="index.php" method="post">
       <input type="text" name="username" value="" placeholder="Username">
       <input type="password" name="password" value="" placeholder="Password">
       <input type="submit" name="login" value="Log in">
     </form>
+
+    <form class="registeruserform" action="login.php" method="post">
+      <input type="submit" name="newuser" value="New user">
+    </form>
+
+    </div>
+
+
 
 </div>
