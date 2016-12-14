8024294eef4cb32fc21657edb84827e8b7cc193d
Erica Glimsholt <ericaglimsholt@hotmail.com>
2016-12-14 13:46:05 +0100

    Created a seperated file for posting a new link and required it

---
+++ blocks/writepost.php
@@ -0,0 +1,28 @@
+<div class="container">
+
+    <?php
+
+    require __DIR__.'/message.php';
+    require __DIR__.'/error.php';
+
+    ?>
+
+    <h1>Share a new link</h1>
+
+    <form name="registerPost" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <input type="hidden" name="action" value="createPost">
+
+        <div class="inputRegister">
+            <p>Subject:</p>
+            <input type="text" name="newSubject" value="">
+        </div>
+
+        <div class="inputRegister">
+            <p>Link:</p>
+            <input type="url" name="newLink" value="">
+        </div>
+
+        <input type="submit" name="linkButton" value="Share link">
+    </form>
+
+</div>
--- home.php
+++ home.php
@@ -15,34 +15,7 @@
 
     <?php if(isset($_SESSION["login"]["uid"])):  ?>
 
-    <div class="container">
-
-        <?php
-
-        require __DIR__.'/blocks/message.php';
-        require __DIR__.'/blocks/error.php';
-
-        ?>
-
-        <h1>Share a new link</h1>
-
-        <form name="registerPost" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <input type="hidden" name="action" value="createPost">
-
-            <div class="inputRegister">
-                <p>Subject:</p>
-                <input type="text" name="newSubject" value="">
-            </div>
-
-            <div class="inputRegister">
-                <p>Link:</p>
-                <input type="url" name="newLink" value="">
-            </div>
-
-            <input type="submit" name="linkButton" value="Share link">
-        </form>
-
-    </div>
+    <?php require __DIR__.'/blocks/writepost.php';?>
 
     <?php endif; ?>
 
