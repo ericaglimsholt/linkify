87edd3361560293ad47bc31c9bab70f76ce676c9
Erica Glimsholt <ericaglimsholt@hotmail.com>
2016-12-19 13:52:36 +0100

    Added author and date to posts and did a simple design to it

--- blocks/new.php
+++ blocks/new.php
@@ -1,5 +1,8 @@
 <div class="container">
+    <div class="post">
     <a target="_blank" href="<?= $post["link"]; ?>"> <h2><?= $post["subject"]; ?> </h2></a>
     <p><?= $post["description"]; ?></p>
+        <h6>Author: <a href="#"><?= $post["username"]; ?></a> | Published: <?= $post["published"]; ?></h6>
 
+    </div>
 </div>
--- home.php
+++ home.php
@@ -20,7 +20,7 @@
     }
 
     // New query for getting all the posts from database
-    $posts = dbGet($connection, "SELECT * FROM posts ORDER BY published DESC");
+    $posts = dbGet($connection, "SELECT * FROM posts, users WHERE posts.uid = users.id ORDER BY published DESC");
 
     // Shows all the posts
     foreach ($posts as $post) {
