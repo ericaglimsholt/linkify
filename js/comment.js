e06e13aabf184b412e6c2ba6bad609869fb80c99
Erica Glimsholt <ericaglimsholt@hotmail.com>
2016-12-27 20:57:37 +0100

    Added comments

--- js/comment.js
+++ js/comment.js
@@ -1,8 +1,10 @@
+// Show the comment field
 document.querySelector('.commentBut').addEventListener("click", function(event){
   document.querySelector('.comments').style.display='flex';
   event.preventDefault();
 });
 
+// Hide the comment field
 document.querySelector(".del").addEventListener("click", function(){
   document.querySelector(".comments").style.display = "none";
 });
