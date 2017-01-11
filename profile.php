2463ca1731a3e8bcc6fbd5c17f9bdfa2a999777c
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-01-11 21:41:21 +0100

    Included all the information about user

--- profile.php
+++ profile.php
@@ -2,6 +2,13 @@
 require __DIR__.'/autoload.php';
 require __DIR__.'/blocks/login.php';
 require __DIR__.'/blocks/head.php';
+
+$information = dbGet($connection, "SELECT * FROM users");
+
+foreach ($information as $info) {
+
+}
+
 ?>
 
 
@@ -13,7 +20,24 @@ require __DIR__.'/blocks/head.php';
 
 // The header
 require __DIR__.'/blocks/header.php';
+
 ?>
+
+<?php if (isset($_SESSION["login"]["uid"])): ?>
+
+    <div class="container">
+        <h1><?= $info["username"] ?>'s profile</h1>
+        <div class="settingAvatar" value=""></div>
+        <div class="information">
+            <p>Name: <?= $info["name"] ?></p>
+            <p>Email: <?= $info["email"] ?></p>
+            <p>Bio: <?= $info["bio"] ?></p>
+        </div>
+
+    </div>
+
+
+<?php endif; ?>
 
 </body>
 
