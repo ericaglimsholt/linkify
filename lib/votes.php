e228554622d6c9af136acbc063277a4f2ea77af3
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-01-17 18:31:41 +0100

    User can downvote and upvote and being saved in database

--- lib/votes.php
+++ lib/votes.php
@@ -8,14 +8,27 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $pid = mysqli_real_escape_string($connection, $_POST["votePid"]);
         $uid = $_SESSION["login"]["uid"];
 
-        if (!dbPost($connection, "INSERT INTO votes (pid, uid, up, down) VALUES ('$pid', '$uid', '$up', '$down')")) {
-            $_SESSION["error"] = "Something went wrong with the database request.";
-            return false;
+        $result = $connection->query("SELECT * FROM votes WHERE pid = $pid");
+        $rowCount = $result->num_rows;
+
+        if ($rowCount > 0) {
+
+            if (!dbPost($connection, "UPDATE votes SET up = '$up' +1 WHERE pid = '$pid'")) {
+                $_SESSION["error"] = "Something went wrong with the database request.";
+                return false;
+            } else {
+                $_SESSION["message"] = "Your upvote update has successfully been updated!";
+            }
+
         } else {
-            $_SESSION["message"] = "Your upvote has successfully been updated!";
+
+            if (!dbPost($connection, "INSERT INTO votes (pid, uid, up, down) VALUES ('$pid', '$uid', '$up', '$down')")) {
+                $_SESSION["error"] = "Something went wrong with the database request.";
+                return false;
+            } else {
+                $_SESSION["message"] = "Your upvote has successfully been updated!";
+            }
         }
-
-
     }
 
     if (isset($_POST["downvote"])) {
@@ -24,20 +37,26 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $pid = mysqli_real_escape_string($connection, $_POST["votePid"]);
         $uid = $_SESSION["login"]["uid"];
 
-        if (!dbPost($connection, "UPDATE votes SET down = '$down' -1 WHERE pid = '$pid'")) {
-            $_SESSION["error"] = "Something went wrong with the database request.";
-            return false;
+        $result = $connection->query("SELECT * FROM votes WHERE pid = $pid");
+        $rowCount = $result->num_rows;
+
+        if ($rowCount > 0) {
+
+            if (!dbPost($connection, "UPDATE votes SET down = '$down' -1 WHERE pid = '$pid'")) {
+                $_SESSION["error"] = "Something went wrong with the database request.";
+                return false;
+            } else {
+                $_SESSION["message"] = "Your downvote update has successfully been updated!";
+            }
+
         } else {
-            $_SESSION["message"] = "Your downvote update has successfully been updated!";
+
+            if (!dbPost($connection, "INSERT INTO votes (pid, uid, down) VALUES ('$pid', '$uid', '$down')")) {
+                $_SESSION["error"] = "Something went wrong with the database request.";
+                return false;
+            } else {
+                $_SESSION["message"] = "Your downvote has successfully been updated!";
+            }
         }
-
-        if (!dbPost($connection, "INSERT INTO votes (pid, uid, down) VALUES ('$pid', '$uid', '$down')")) {
-            $_SESSION["error"] = "Something went wrong with the database request.";
-            return false;
-        } else {
-            $_SESSION["message"] = "Your downvote has successfully been updated!";
-        }
-
-
     }
 }
