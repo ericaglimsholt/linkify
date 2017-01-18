9568347b984c1cbd78235189d90bcc73d311e8c9
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-01-18 07:07:06 +0100

    Now you delete votes, comments and the whole post

--- lib/delete.php
+++ lib/delete.php
@@ -4,6 +4,23 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $pid = mysqli_real_escape_string($connection, $_POST["deletePid"]);
 
+    // Deleting votes connected to post
+    if (!dbPost($connection, "DELETE FROM comments WHERE pid = '$pid';")) {
+        $_SESSION["error"] = "Something went wrong with the database request.";
+        return false;
+    } else {
+        $_SESSION["message"] = "Your post has successfully been deleted!";
+    }
+
+    // Deleting votes connected to votes
+    if (!dbPost($connection, "DELETE FROM votes WHERE pid = '$pid';")) {
+        $_SESSION["error"] = "Something went wrong with the database request.";
+        return false;
+    } else {
+        $_SESSION["message"] = "Your post has successfully been deleted!";
+    }
+
+    // Deleting post
     if (!dbPost($connection, "DELETE FROM posts WHERE id = '$pid';")) {
         $_SESSION["error"] = "Something went wrong with the database request.";
         return false;
