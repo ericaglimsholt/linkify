8f77a845923d296046d94c341d25e0b306dfbaa8
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-01-18 18:24:50 +0100

    Restored comments js file and deleted votes

---
+++ js/comment.js
@@ -0,0 +1,20 @@
+// Show the comment field
+const showed = document.querySelectorAll('.commentBut');
+showed.forEach(function(showing)
+{
+    const parent = showing.parentElement.parentElement;
+    showing.addEventListener("click", function(event){
+        parent.querySelector('.comments').style.display='flex';
+        event.preventDefault();
+    });
+});
+
+// Hide the comment field
+const hide = document.querySelectorAll('.del');
+hide.forEach(function(deleting)
+{
+    const parent = deleting.parentElement.parentElement;
+    deleting.addEventListener("click", function(event){
+        parent.querySelector(".comments").style.display = "none";
+    });
+});
