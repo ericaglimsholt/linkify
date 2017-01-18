022e80397cd3bfcfba1848dd26849093fcfd895b
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-01-18 08:02:23 +0100

    Deleted some files that wasnt being used

--- js/delete.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.deleteBut').addEventListener("click", function() {
-
-  if (confirm('Are you sure you want to delete your post?')) {
-    document.querySelector('.post').style.display='none';
-  } else {
-      // Do nothing!
-  }
-
-});
